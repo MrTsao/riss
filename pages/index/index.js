3abea5a1445ba779b414f10d9cc548781bda8fa0
MrTsao <rcode@163.com>
2017-10-25 23:46:47 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,55 @@
+//index.js
+//获取应用实例
+var app = getApp();
+var util = require('../../utils/util.js');
+Page({
+  data: {
+    PAGE: "INDEX",
+    userInfo: {},
+    height: 0,
+    width: 0,
+    news: [{ CPID: '001', CNME: '党的十九届一中全会选举习近平为中央委员会总书记' }, { CPID: '002', CNME: '水利部召开2017年第8次中央水利投资计划执行月调度会商会议' }, { CPID: '003', CNME: '水利部着力推进南水北调工程管理范围和保护范围划定工作' }, { CPID: '004', CNME: '三峡水库连续八年实现175米试验性蓄水目标 国家防总长江防总科学调度水库群实现多赢' }, { CPID: '005', CNME:'水利部党组坚决兑现庄严政治承诺 如期实现巡视全覆盖'}],
+    COLOR: ['#6699cc', '#778899', '#99cc66', '#5F9EA0', '#8FBC8F', '#BDB76B'],
+    goodrivers: [{ CPID: '001', CNME: '滠水河' }, { CPID: '002', CNME: '金水河' }, { CPID: '003', CNME: '断琴小河' }],
+    riverinfo: [{ CPID: '001', CNME: '长江武汉段' }, { CPID: '002', CNME: '汉江武汉段' }, { CPID: '003', CNME: '府环河' }, { CPID: '004', CNME: '滠水河' }],
+    feedbacks: [{ CPID: '001', CNME: '长江武汉段大量漂浮物未清理' }, { CPID: '003', CNME: '府环河水质变差' }],
+    workitems: [{ CPID: '001', CNME: '长江武汉段大量漂浮物已安排清理' }, { CPID: '003', CNME: '府环河水质变差情况说明' }],
+    COLOR: ['#6699cc', '#778899', '#99cc66', '#5F9EA0', '#8FBC8F', '#BDB76B']
+  },
+  //事件处理函数
+  bindViewTap: function () {
+  },
+  //展开或隐藏菜单
+  menucontrol: function (e) {
+    this.setData({
+      menuhide: !this.data.menuhide
+    })
+  },
+  subitemcontrol: function (e) {
+    this.setData({
+      menuIndex: e.currentTarget.dataset.idx
+      , menuhide: !this.data.menuhide
+    });
+  },
+  onLoad: function () {
+    var SysInfo = wx.getSystemInfoSync()
+    util.Post(this, "LOAD", null, function (that, data) {
+      that.setData({
+        height: SysInfo.windowHeight,
+        width: SysInfo.screenWidth,
+        chapters: data.CHAPTER
+      })
+    });
+  },
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    var that = this
+    return {
+      title: "亚润河长",
+      desc: '',
+      path: '/pages/index/index'
+    }
+  }
+})
---
+++ utils/util.js
@@ -0,0 +1,245 @@
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+function formatString(n) {
+  var leave = n % (12 * 30 * 24 * 3600)
+  var months = Math.floor(leave / (30 * 24 * 3600))
+  // //计算出相差天数
+  var leave0 = leave % (30 * 24 * 3600)
+  var days = Math.floor(leave0 / (24 * 3600))
+  // //计算出小时数
+  var leave1 = leave0 % (24 * 3600)     //计算天数后剩余的毫秒数
+  var hours = Math.floor(leave1 / (3600))
+  // //计算相差分钟数
+  var leave2 = leave1 % (3600)         //计算小时数后剩余的毫秒数
+  var minutes = Math.floor(leave2 / (60))
+  // //计算相差秒数
+  var leave3 = leave2 % (60)    //计算分钟数后剩余的毫秒数
+  var seconds = Math.round(leave3)
+  var strFormat = ""
+  if (months > 0) { strFormat += formatNumber(months) + "个月" }
+  if (days > 0) { strFormat += formatNumber(days) + "天" }
+  if (hours > 0) { strFormat += formatNumber(hours) + "小时" }
+  if (minutes > 0) { strFormat += formatNumber(minutes) + "分" }
+  if (seconds > 0) { strFormat += formatNumber(seconds) + "秒" }
+  if (strFormat == "") { strFormat = "0秒" }
+  return strFormat
+}
+
+function formatTimeString(n) {
+  var leave = n % (12 * 30 * 24 * 3600)
+  var months = Math.floor(leave / (30 * 24 * 3600))
+  // //计算出相差天数
+  var leave0 = leave % (30 * 24 * 3600)
+  var days = Math.floor(leave0 / (24 * 3600))
+  // //计算出小时数
+  var leave1 = leave0 % (24 * 3600)     //计算天数后剩余的毫秒数
+  var hours = Math.floor(leave1 / (3600))
+  // //计算相差分钟数
+  var leave2 = leave1 % (3600)         //计算小时数后剩余的毫秒数
+  var minutes = Math.floor(leave2 / (60))
+  // //计算相差秒数
+  var leave3 = leave2 % (60)    //计算分钟数后剩余的毫秒数
+  var seconds = Math.round(leave3)
+  var strFormat = ""
+  if (months > 0) { strFormat = months.toString() + "个月前" }
+  else if (days > 0) { strFormat = days.toString() + "天前" }
+  else if (hours > 0) { strFormat = hours.toString() + "小时前" }
+  else if (minutes > 0) { strFormat = minutes.toString() + "分钟前" }
+  else if (seconds > 0 || strFormat == "") { strFormat = "刚刚" }
+  return strFormat
+}
+
+function imageUtil(e) {
+  var imageSize = {};
+  var originalWidth = e.detail.width;//图片原始宽 
+  var originalHeight = e.detail.height;//图片原始高 
+  var originalScale = originalHeight / originalWidth;//图片高宽比 
+  console.log('originalWidth: ' + originalWidth)
+  console.log('originalHeight: ' + originalHeight)
+  //获取屏幕宽高 
+  wx.getSystemInfo({
+    success: function (res) {
+      var windowWidth = res.windowWidth;
+      var windowHeight = res.windowHeight;
+      var windowscale = windowHeight / windowWidth;//屏幕高宽比 
+      console.log('windowWidth: ' + windowWidth)
+      console.log('windowHeight: ' + windowHeight)
+      if (originalScale < windowscale) {//图片高宽比小于屏幕高宽比 
+        //图片缩放后的宽为屏幕宽 
+        imageSize.imageWidth = windowWidth;
+        imageSize.imageHeight = (windowWidth * originalHeight) / originalWidth;
+      } else {//图片高宽比大于屏幕高宽比 
+        //图片缩放后的高为屏幕高 
+        imageSize.imageHeight = windowHeight;
+        imageSize.imageWidth = (windowHeight * originalWidth) / originalHeight;
+      }
+
+    }
+  })
+  console.log('缩放后的宽: ' + imageSize.imageWidth)
+  console.log('缩放后的高: ' + imageSize.imageHeight)
+  return imageSize;
+}
+
+/**
+* url 请求地址
+* success 成功的回调
+* fail 失败的回调
+*/
+function _post_json(jsPost, success, fail) {
+  let app = getApp();
+  var data = jsPost || new jsonRow()
+  data.AddCell("OPEN_KEY", app.globalData.openData.OPEN_KEY)
+  wx.showNavigationBarLoading()
+  wx.request({
+    url: app.globalData.url,
+    header: {
+      'content-type': 'application/json',
+      'Cookie': 'ASP.NET_SessionId=' + app.globalData.openData.SESSION_ID
+    },
+    method: 'POST',
+    data: data.GetStr(),
+    success: function (res) {
+      //wx.hideToast()
+      wx.hideNavigationBarLoading()
+      if (res.data.msg == "err") {
+        console.log(res.data.data);
+      } else {
+        if (res.data.msg == "NO_SESSION") {
+          wx.setStorageSync('openkey', null);
+          wx.setStorageSync('code', '');
+          app.getUserInfo(function () {
+            app.getOpenInfo(function () {
+              _post_json(jsPost, success, fail)
+            });
+          });
+        } else if (res.data.msg == "NO_USER") {
+          app.getUserInfo(null, function (user) {
+            _newUserId(app.globalData.url, user, app.globalData.openData, function () {
+              _post_json(jsPost, success, fail)
+            })
+          })
+        } else {
+          if (res.data.msg != "") {
+            wx.showToast({
+              title: res.data.msg || "错误"
+            })
+          }
+          success(res);
+        }
+      }
+    },
+    fail: function (res) {
+      console.log(res);
+    }
+  });
+  //console.log("----end----_post-----");
+}
+
+function jsonRow() { this.arrjson = {}; }
+jsonRow.prototype = {
+  AddCell: function (sName, sValue) {
+    if (!sName) return; this.arrjson[sName] = sValue;
+  },
+  GetStr: function () {
+    var _strjson = "";
+    for (var m in this.arrjson) {
+      if (_strjson == "") { _strjson = "\"" + m + "\":\"" + this.arrjson[m] + "\""; } else { _strjson += ",\"" + m + "\":\"" + encodeURIComponent(this.arrjson[m]) + "\""; }
+    }
+    if (_strjson == "") return ""; else return "{" + _strjson + "}";
+  }
+};
+
+//服务器请求数据
+function Post(that, action, data, doAfter) {
+  //数据请求执行方法
+  var app = getApp();
+  app.getOpenInfo(function () {
+    var jsPost = data || new jsonRow()
+    var ppage = that.data.PAGE || "BseHandler"
+    jsPost.AddCell("PAGE", ppage)
+    jsPost.AddCell("ACTION", action)
+    _post_json(jsPost, function (res) {
+      typeof doAfter == "function" && doAfter(that, res.data.data)
+    })
+  })
+}
+
+function _getOpenId(url, rescode, doAfter) {
+  var jsPost = new jsonRow()
+  jsPost.AddCell("PAGE", "BseHandler")
+  jsPost.AddCell("ACTION", "OPENID")
+  jsPost.AddCell("CODE", rescode)
+  wx.request({
+    url: url,
+    header: {
+      'content-type': 'application/json'
+    },
+    method: 'POST',
+    data: jsPost.GetStr(),
+    success: function (res) {
+      if (res.data.msg == "err") {
+        console.log(res.data.data);
+      } else {
+        typeof doAfter == "function" && doAfter(res.data)
+      }
+    },
+    fail: function (res) {
+      console.log(res);
+    }
+  });
+}
+
+
+function _newUserId(url, user, openData, doAfter) {
+  var jsPost = new jsonRow()
+  jsPost.AddCell("PAGE", "REGIESTHANDLER")
+  jsPost.AddCell("ACTION", "NEW")
+  jsPost.AddCell("USER_NME", user.nickName)
+  jsPost.AddCell("HEAD_IMG", user.avatarUrl)
+  jsPost.AddCell("OPEN_KEY", openData.OPEN_KEY)
+  wx.request({
+    url: url,
+    header: {
+      'content-type': 'application/json',
+      'Cookie': 'ASP.NET_SessionId=' + openData.SESSION_ID
+    },
+    method: 'POST',
+    data: jsPost.GetStr(),
+    success: function (res) {
+      if (res.data.msg == "err") {
+        console.log(res.data.data);
+      } else {
+        typeof doAfter == "function" && doAfter(res.data)
+      }
+    },
+    fail: function (res) {
+      console.log(res);
+    }
+  });
+}
+
+module.exports = {
+  formatTime: formatTime,
+  formatTimeString: formatTimeString,
+  formatString: formatString,
+  imageUtil: imageUtil,
+  Post: Post,
+  jsonRow: jsonRow,
+  getOpenId: _getOpenId
+}
