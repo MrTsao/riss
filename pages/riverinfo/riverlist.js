a13fe2ecaaaeb5099e871d77bc60b3cf650650b5
MrTsao <rcode@163.com>
2017-10-27 00:27:17 +0800

    add base pages

---
+++ pages/riverinfo/riverlist.js
@@ -0,0 +1,67 @@
+// pages/riverinfo/riverlist.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    riverinfo: [{ CPID: '001', CNME: '长江武汉段' }, { CPID: '002', CNME: '汉江武汉段' }, { CPID: '003', CNME: '府环河' }, { CPID: '004', CNME: '滠水河' }],
+    COLOR: ['#6699cc', '#778899', '#99cc66', '#5F9EA0', '#8FBC8F', '#BDB76B']
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
